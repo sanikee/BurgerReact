16da278221b4c0f8482fd90b63b233543c722e88
sanikee <s-a-n-i-k-a@yandex.ru>
2023-02-22 02:17:16 +0600

    add Header & Container

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import { Container } from "./components/Container/Container"
+import { Header } from './components/Header/Header.jsx'
+
+
+export const App = () => {
+  
+
+  return (
+    <>
+    <Header />
+    <main>
+      <nav>
+        <Container />
+        </nav>
+      <section></section>
+    </main>
+    <footer></footer>
+    </>
+  )
+}
+
+
