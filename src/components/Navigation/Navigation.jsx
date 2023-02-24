b77c7c68586eae52a7f4afbd8187428a501d53ef
sanikee <s-a-n-i-k-a@yandex.ru>
2023-02-24 17:43:08 +0600

    API, categories, products, cart localStorage

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,24 +2,29 @@ import classNames from 'classnames';
 import { Container } from '../Container/Container.jsx';
 import style from './Navigation.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeCategory } from '../../store/category/categorySlice.js';
+import { categoryRequestAsync, changeCategory } from '../../store/category/categorySlice.js';
+import { useEffect } from 'react';
+import { API_URI } from '../../const.js';
 
 export const Navigation = () => {
-    const { category, activeCategory } = useSelector((state) => state.category )
+    const { category, activeCategory } = useSelector((state) => state.category)
     const dispatch = useDispatch()
 
+    useEffect(() => {
+        dispatch(categoryRequestAsync());
+    }, [])
 
     return (
         <nav className={style.navigation}>
             <Container className={style.container}>
                 <ul className={style.list}>
                     {category.map((item, i) => (
-                        <li className={style.item}>
+                        <li className={style.item} key={item.title}>
                             <button 
                                 className={classNames(
                                     style.button, 
                                     activeCategory === i ? style.button_active : '')}
-                                    style={{ backgroundImage: `url(${item.image})`}} 
+                                    style={{ backgroundImage: `url(${API_URI}/${item.image})`}} 
                                     onClick={() => {
                                         dispatch(changeCategory({indexCategory: i}))
                                     }}
